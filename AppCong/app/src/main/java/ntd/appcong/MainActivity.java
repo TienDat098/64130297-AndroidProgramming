473db5d5e5766c1b6b00ddfbf1826221ec30da64
NgTienDat <dat.nti.64cntt@ntu.edu.vn>
2025-02-24 21:18:41 +0700

    update với xử lý sk click

--- AppCong/app/src/main/java/ntd/appcong/MainActivity.java
+++ AppCong/app/src/main/java/ntd/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package ntd.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //Gắn layout tương ứng với file này
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ConstraintLayout), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        }                       );
+    }
+    //Đây là bộ lắng nghe v xử lý sự kiện click lên nút Tính Tổng
+    public void XuLyCong(View view){
+//Tìm , tham chiếu đến điều khiển trên tệp XML
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+        editTextKetQua.setEnabled(false); // Vô hiệu hóa nhập liệu
+        //Lấy dữ liệu về ở điều khiển số a
+         String strA =editTextSoA.getText().toString();//strA="2"
+        //Lấy dữ liệu về ở điều khiển số b
+        String strB =editTextSoB.getText().toString();//strB="4"
+        //Chuyển dữ liệu sang dạng số
+        int so_A=Integer.parseInt(strA);//2
+        int so_B=Integer.parseInt(strB);//4
+        //Tính toán theo yêu cầu
+        int tong=  so_A + so_B;//6
+        //Hiện ra màn hình
+        String strTong =String.valueOf(tong);//chuyển sang dạng chuỗi "6"
+        editTextKetQua.setText(strTong);
+
     }
 }
