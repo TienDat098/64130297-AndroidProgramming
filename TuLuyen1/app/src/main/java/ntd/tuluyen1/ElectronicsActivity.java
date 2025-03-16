6989e39a18b1f09becec2a517cae5682d6dc2058
NgTienDat <dat.nti.64cntt@ntu.edu.vn>
2025-03-16 20:14:07 +0700

    TuLuyen1
    chua xong

--- TuLuyen1/app/src/main/java/ntd/tuluyen1/ElectronicsActivity.java
+++ TuLuyen1/app/src/main/java/ntd/tuluyen1/ElectronicsActivity.java
@@ -1,9 +1,6 @@
 package ntd.tuluyen1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ClothingActivity extends AppCompatActivity {
+public class ElectronicsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clothing);
+        setContentView(R.layout.activity_electronics);
 
     }
 }
--- TuLuyen1/app/src/main/java/ntd/tuluyen1/MainActivity.java
+++ TuLuyen1/app/src/main/java/ntd/tuluyen1/MainActivity.java
@@ -12,14 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    CardView clothingCard;
+    CardView clothingCard,elecCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         clothingCard=findViewById(R.id.clothingCard);
-
+        elecCard=findViewById(R.id.electronicCard);
         clothingCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+    elecCard.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent1 =new Intent(MainActivity.this,ElectronicsActivity.class);
+            startActivity(intent1);
+        }
+    });
     }
 }
