6846c022993b498c1aacecdf25afd6c8d410e58d
NgTienDat <dat.nti.64cntt@ntu.edu.vn>
2025-03-20 18:58:06 +0700

    update màn hình 2

--- Figma_dongian1/app/src/main/java/com/example/figma_dongian1/MainActivity.java
+++ Figma_dongian1/app/src/main/java/com/example/figma_dongian1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.figma_dongian1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView image1;
 
+    void TimDieuKhien(){
+        image1=findViewById(R.id.img1);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TimDieuKhien();
+    image1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent(MainActivity.this,ModesActivity.class);
+            startActivity(intent);
+        }
+    });
     }
+
 }
